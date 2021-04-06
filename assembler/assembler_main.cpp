d77a2706c40e6dba5573303269c5bcc77d42bc65
Stanislav Kurnevich <70342945+StanislavKy@users.noreply.github.com>
2021-04-06 13:27:00 +0300

    Update assembler_main.cpp

--- assembler/assembler_main.cpp
+++ assembler/assembler_main.cpp
@@ -1,9 +1,27 @@
 #include "assembler.h"
 
-int main ()
+int main (int argc, char* argv[])
 {
     struct file asm_file = {};
-    char   file_name[17] = "../txt files/asm";
+    //char   file_name[17] = "../txt files/asm";
+    char* file_name = "";
+    
+    if (argc == 2)
+    {
+        file_name = argv[1];
+        printf ("You want read \"%s\"\n", file_name);
+    }
+    else if (argc == 1)
+    {
+        printf ("You didn't enter the file name. Please return program and enter file name.\n");
+        return 0;
+    }
+    else
+    {
+        printf ("Error to few or too many arguments.\n");
+        return 0;
+    }
+
     struct str* data     = assembler_read (&asm_file, file_name);
   
     assembler_free (asm_file.file_buffer, data);
