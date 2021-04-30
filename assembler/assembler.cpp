409de8ce1180bee987a7cddab89ff697cf1889ff
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-04-30 21:22:28 +0300

    Update assembler.cpp

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -13,20 +13,20 @@ void assembler_read (text* asm_file, char* file_name)
     labels* label = (labels*) calloc (asm_file -> number_lexems, sizeof (label));
     assert (label);
     
-    asm_cmd::firs_passing   (asm_file, label);
-    asm_cmd::second_passing (asm_file, label);
+    int nmb_flags = asm_cmd::firs_passing (asm_file, label);
+    asm_cmd::second_passing (asm_file, label, nmb_flags);
 }
 
 //=====================================================================================================
 
-void asm_cmd::firs_passing (text* asm_file, labels* label)
+int asm_cmd::firs_passing (text* asm_file, labels* label)
 {
     assert (asm_file);
     assert (asm_file -> lexem);
     assert (asm_file -> file_buffer);
    
     int flag_counter = 0;  
-    int nmb_flags = 0;
+    int nmb_flags    = 0;
     int nmb_lexems   = asm_file -> number_lexems;
 
     double number    = 0; 
@@ -44,7 +44,9 @@ void asm_cmd::firs_passing (text* asm_file, labels* label)
         {
             number = atof (cmd);
             printf ("number is %lg\n", number);
-            (asm_file -> lexem + i) -> lexem_type = NUMBER;
+            (asm_file -> lexem + i) -> lexem_type = NUMBER; 
+            printf ("\n1lexem type is %d\n", (asm_file -> lexem + i) -> lexem_type);
+            printf ("1 i is %d\n", i);
         }
 
         else 
@@ -54,9 +56,7 @@ void asm_cmd::firs_passing (text* asm_file, labels* label)
 
             if (!strcmp (cmd, "push") || !strcmp (cmd, "pop"))
             {
-                printf ("\t\t\tCMD is %s\n", cmd);
                 nmb_flags = flags_input (asm_file, nmb_lexems, i, nmb_flags);
-                printf ("\t\tNUMBER flagsss IS %d\n", nmb_flags);
             }
             if (lexem[lex_len - 1] == ':')
             {
@@ -64,11 +64,11 @@ void asm_cmd::firs_passing (text* asm_file, labels* label)
                 lexem[lex_len - 1] = '\0';
 
                 label -> label_name = lexem;
-                printf ("\t\tNUMBER FLAGS IS %d\n", nmb_flags);
                 label -> ip = i + nmb_flags;
             }
         }
     }
+    return nmb_flags;
 }
     
 
@@ -85,11 +85,11 @@ int flags_input (text* asm_file, int nmb_lex, int i, int nmb_flags)
         char* next_lex = (asm_file -> lexem + i + 1) -> lexem_name;
 
         if (isdigit (*next_lex)) // если след лексема число то это не регистр
-            (asm_file -> lexem + i) -> lexem_type = REG_COMMAND;
+            (asm_file -> lexem + i) -> lexem_type = NMB_COMMAND;
 
         else if (*(next_lex) == 'r') //если так то это регистр
         {    
-            (asm_file -> lexem + i) -> lexem_type = NMB_COMMAND;
+            (asm_file -> lexem + i) -> lexem_type = REG_COMMAND;
             nmb_flags++;
         }
     }
@@ -99,7 +99,7 @@ int flags_input (text* asm_file, int nmb_lex, int i, int nmb_flags)
 
 //=====================================================================================================
 
-void asm_cmd::second_passing (text* asm_file, labels* label)
+void asm_cmd::second_passing (text* asm_file, labels* label, int nmb_flags)
 {
     assert (asm_file);
     assert (asm_file -> lexem);
@@ -110,29 +110,33 @@ void asm_cmd::second_passing (text* asm_file, labels* label)
 
     int nmb_lexems = asm_file -> number_lexems;
     int number = 0;
-    
-    for (int i = 0; i < nmb_lexems; i++)
+    int offset_counter = 0;
+    int endif = 0;
+    for (int i = 0; i < nmb_lexems + nmb_flags + 2; i++)
     {
-        char* lexem_name = (asm_file -> lexem + i) -> lexem_name;
-        int   lexem_type = (asm_file -> lexem + i) -> lexem_type;
+        char* lexem_name = (asm_file -> lexem + i - offset_counter) -> lexem_name;
+        int   lexem_type = (asm_file -> lexem + i - offset_counter) -> lexem_type;
 
         if (lexem_type == NUMBER)
         {
             number = atof (lexem_name);
-            byte_code [i] = asm_cmd::assembling (asm_file, nullptr, number, asm_file -> lexem + i);
+            byte_code [i] = asm_cmd::assembling (asm_file, nullptr, number);
         }
 
         else 
         {
-            if ((asm_file -> lexem + i) -> lexem_type == LABEL)
+            if ((asm_file -> lexem + i - offset_counter) -> lexem_type == LABEL)
                 continue; 
 
-            byte_code [i] = asm_cmd::assembling (asm_file, lexem_name, 0, asm_file -> lexem + i);
+            byte_code [i] = asm_cmd::assembling (asm_file, lexem_name, 0);
             
-            i = placing_flags (asm_file, label, byte_code, nmb_lexems, i);
-
+            int temp = i;
+            i = placing_flags (asm_file, label, byte_code, nmb_lexems, i, offset_counter);
+            if (i != temp)
+                offset_counter++;
+            
             if (byte_code[i] == JMP)
-                i = label_input (asm_file, label, byte_code, (asm_file -> lexem + i + 1) -> lexem_name, i); 
+                i = label_input (asm_file, label, byte_code, (asm_file -> lexem + i + 1 - offset_counter) -> lexem_name, i); 
         }
     }
 
@@ -140,13 +144,14 @@ void asm_cmd::second_passing (text* asm_file, labels* label)
     free (byte_code);
 }
 
+
 //=====================================================================================================
 
-int placing_flags (text* asm_file, labels* label, double* byte_code, int nmb_lexems, int i)
+int placing_flags (text* asm_file, labels* label, double* byte_code, int nmb_lexems, int i, int offsets_counter)
 {
     if (i < nmb_lexems - 1)
     {
-        int lex_type = (asm_file -> lexem + i) -> lexem_type;
+        int lex_type = (asm_file -> lexem + i - offsets_counter) -> lexem_type;
 
         if (lex_type == REG_COMMAND) // если след лексема число то это не регистр
         {
@@ -190,31 +195,33 @@ int label_input (text* asm_file, labels* label, double* byte_code, char* next_cm
 
 //=====================================================================================================
 
-double asm_cmd::assembling (text* asm_file, char* cmd, int number, lexemes* lexem_i)
+double asm_cmd::assembling (text* asm_file, char* cmd, int number)
 {
     if (cmd != nullptr)
     {
         if (false) {} 
 
-        GET_COMMAND (push, PUSH, 0)
-        GET_COMMAND (pop, POP, 0)
-        GET_COMMAND (add, ADD, 0)
-        GET_COMMAND (sub, SUB, 0)
-        GET_COMMAND (div, DIV, 0)
-        GET_COMMAND (mul, MUL, 0)
-        GET_COMMAND (out, OUT, 0)
-        GET_COMMAND (hlt, HLT, 0)
-        GET_COMMAND (end, END, 0)
-        GET_COMMAND (rax, RAX, 0)
-        GET_COMMAND (rbx, RBX, 0)
-        GET_COMMAND (rcx, RCX, 0)
-        GET_COMMAND (rdx, RDX, 0)
-        GET_COMMAND (jmp, JMP, 0)
-        GET_COMMAND (unknown_cmd, UNKNOWN_CMD, 0)
+        GET_COMMAND (push, PUSH)
+        GET_COMMAND (pop,   POP)
+        GET_COMMAND (add,   ADD)
+        GET_COMMAND (sub,   SUB)
+        GET_COMMAND (div,   DIV)
+        GET_COMMAND (mul,   MUL)
+        GET_COMMAND (out,   OUT)
+        GET_COMMAND (hlt,   HLT)
+        GET_COMMAND (end,   END)
+        GET_COMMAND (rax,   RAX)
+        GET_COMMAND (rbx,   RBX)
+        GET_COMMAND (rcx,   RCX)
+        GET_COMMAND (rdx,   RDX)
+        GET_COMMAND (jmp,   JMP)
+        GET_COMMAND (unknown_cmd, UNKNOWN_CMD)
     }
 
-    else if (number != 0)             
-        return number;                        
+    else if (cmd == nullptr) 
+    {
+        return number;  
+    }                                  
     else 
         printf ("\t\tError cmd is {%s}\n\n", cmd);
     
