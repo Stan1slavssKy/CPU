6dfbe2fbb6b6f8b1efbd8d75ce2dec44a698a150
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-05-03 16:44:59 +0300

    Add files via upload

--- cpu/cpu.h
+++ cpu/cpu.h
@@ -27,26 +27,50 @@ struct CPU_t
     double*    byte_code;
 
     struct Regist*  regs;
+    struct cmp_flag* flag;
 };
 
 struct Regist
 {
-    int rax = 0;
-    int rbx = 0;
-    int rcx = 0;
-    int rdx = 0;
+    double rax = 0;
+    double rbx = 0;
+    double rcx = 0;
+    double rdx = 0;
 };
+
+struct cmp_flag
+{
+    int ZF = 0;
+    int SF = 0;
+};
+
+//===========================================================================  
+
+#define ARIFMETICAL_CMD(command, operation)                          \
+{                                                                    \
+            x1  = stack_pop (cpu -> stack);                          \
+            x2  = stack_pop (cpu -> stack);                          \
+            res = x2 operation x1;                                   \
+                                                                     \
+            stack_push (cpu -> stack, res);                          \
+            fprintf (cpu_file, "\t\t"#command" %lg %lg\n", x2, x1);  \
+            cpu_print (cpu, cpu_file);                               \
+}                                                                    \
+
+#define RESET_FLAGS                  \
+            (cpu -> flag) -> ZF = 0; \
+            (cpu -> flag) -> SF = 0; \
 
 //===========================================================================
 
 void CPU_construct (CPU_t* cpu, char* file_name);
-void CPU_read_file (CPU_t* cpu, char* file_name);
+int  CPU_read_file (CPU_t* cpu, char* file_name);
 void CPU_destruct  (CPU_t* cpu);
 void cpu_print     (CPU_t* cpu, FILE* cpu_file);
 
 namespace asm_cmd
 {   
-    int determine_commands (CPU_t* cpu);
+    int determine_commands (CPU_t* cpu, int size_of_file);
 }
 
 const char* reg_push (CPU_t* cpu, double reg);
