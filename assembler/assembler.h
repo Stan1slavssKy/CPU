054a1f7eb694360efbf25b61bbd73faeb5338000
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-05-03 16:44:21 +0300

    Add files via upload

--- assembler/assembler.h
+++ assembler/assembler.h
@@ -15,17 +15,11 @@
 
 //=================================================================================
 
-#define GET_COMMAND(cmd_compared, CMD_ENUM, number)               \
+#define GET_COMMAND(cmd_compared, CMD_ENUM)                       \
     else if (!strcmp(cmd, #cmd_compared))                         \
     {                                                             \
         return CMD_ENUM;                                          \
     }
-
-
-#define TYPE_COMMAND(CMD_ENUM, LEX_TYPE)     \
-    case CMD_ENUM:                           \
-        lexem_i -> lexem_type = LEX_TYPE;    \
-        break;
 
 //=================================================================================
 
@@ -37,9 +31,9 @@ struct labels
 
 //=================================================================================
 
-int flags_input    (text* asm_file, int nmb_lex, int i, int nmb_flags);
-int  placing_flags (text* asm_file, labels* label, double* byte_code, int nmb_lexems, int i);
-int  label_input   (text* asm_file, labels* label, double* byte_code, char* next_cmd, int i);
+int flags_input   (text* asm_file, int nmb_lex, int i, int nmb_flags);
+int placing_flags (text* asm_file, labels* label, double* byte_code, int nmb_lexems, int idx, int i);
+int label_input   (text* asm_file, labels* label, double* byte_code, char* next_cmd, int i);
 
 void assembler_read (text* asm_file, char* file_name);
 void input_b_file   (text* asm_file, double* byte_code);
@@ -47,9 +41,9 @@ void assembler_free (text* asm_file);
 
 namespace asm_cmd
 {
-    void   firs_passing   (text* asm_file, labels* label);
-    void   second_passing (text* asm_file, labels* label);
-    double assembling     (text* asm_file, char* cmd, int number, lexemes* lexem_i);
+    int    firs_passing   (text* asm_file, labels* label);
+    void   second_passing (text* asm_file, labels* label, int nmb_flags);
+    double assembling     (text* asm_file, char* cmd, int number);
 
 }
 
