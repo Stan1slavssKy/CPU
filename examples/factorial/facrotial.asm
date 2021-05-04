dfbcf375abec98acdbbb83dc40b18889cb8de3a8
Stanislav Kurnevich <70342945+Stan1slavssKy@users.noreply.github.com>
2021-05-04 12:56:47 +0300

    Update facrotial.asm

--- examples/factorial/facrotial.asm
+++ examples/factorial/facrotial.asm
@@ -1,1 +1,26 @@
+        push 1
+        pop rbx  
 
+        push 12
+        pop rax
+
+factorial:
+
+        push rbx
+        push rax 
+
+        mul
+        pop rbx
+
+        dec rax
+
+        push rax
+        push 0
+        
+        cmp 
+        jne factorial
+        
+        push rbx
+        out
+
+        end
